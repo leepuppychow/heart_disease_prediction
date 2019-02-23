87f61cc35266d8b18a1ee3afa613436ddcce6e91
leepuppychow <leepuppychow@gmail.com>
2019-02-23 14:46:48 -0700

    Serves up index.html

--- server/main.go
+++ server/main.go
@@ -1,5 +1,17 @@
 package main
 
-func main() {
+import (
+	"log"
+	"net/http"
+)
 
+func IndexHandler(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, "./static/index.html")
+}
+
+func main() {
+	http.HandleFunc("/", IndexHandler)
+	port := ":8000"
+	log.Println("Server running on port", port)
+	log.Fatal(http.ListenAndServe(port, nil))
 }
