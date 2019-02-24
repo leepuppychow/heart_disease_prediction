08173820177825581b28ad8ee271eedfb98aeda8
leepuppychow <leepuppychow@gmail.com>
2019-02-24 12:47:57 -0700

    TEst commit

--- server/main.go
+++ server/main.go
@@ -1,18 +1,13 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"net/http"
 
-	db "github.com/leepuppychow/heart_disease_prediction/server/database"
 	h "github.com/leepuppychow/heart_disease_prediction/server/handler"
 )
 
 func main() {
-	fmt.Println(db.Exec("HMSET", "hash", "name", "lee", "age", "30"))
-	fmt.Println(db.Exec("HGETALL", "hash"))
-
 	http.HandleFunc("/", h.IndexHandler)
 	http.HandleFunc("/patients", h.NewPatientHandler)
 	port := ":8000"
