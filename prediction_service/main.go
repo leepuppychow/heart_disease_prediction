890e4f5274a0610d60a12add04b0cdc5b456b39f
leepuppychow <leepuppychow@gmail.com>
2019-02-23 22:56:06 -0700

    Moving database package to root level

--- prediction_service/main.go
+++ prediction_service/main.go
@@ -3,7 +3,7 @@ package main
 import (
 	"fmt"
 
-	db "github.com/leepuppychow/heart_disease_prediction/prediction_service/database"
+	db "github.com/leepuppychow/heart_disease_prediction/database"
 )
 
 func main() {
