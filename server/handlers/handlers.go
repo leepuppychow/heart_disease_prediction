30dac1f196ef79372b9bac4f7d1f47dde13b3407
leepuppychow <leepuppychow@gmail.com>
2019-03-02 00:00:08 -0700

    Form is dynamic and will send Train/Predict message to service

--- server/handlers/handlers.go
+++ server/handlers/handlers.go
@@ -1,9 +1,12 @@
 package handlers
 
 import (
+	"log"
 	"net/http"
+	"strings"
 
 	db "github.com/leepuppychow/heart_disease_prediction/server/database"
+	"github.com/leepuppychow/heart_disease_prediction/server/messages"
 )
 
 func IndexHandler() http.Handler {
@@ -12,9 +15,22 @@ func IndexHandler() http.Handler {
 
 func NewPatientHandler(w http.ResponseWriter, r *http.Request) {
 	age := r.FormValue("age")
-	gender := r.FormValue("gender")
+	sex := r.FormValue("sex")
 	cp := r.FormValue("cp")
-	row := age + "," + gender + "," + cp
-	db.AddRow(row)
+	trestbps := r.FormValue("trestbps")
+	chol := r.FormValue("chol")
+	fbs := r.FormValue("fbs")
+
+	hasHeartDisease := r.FormValue("hasHeartDisease")
+
+	if hasHeartDisease == "" {
+		messages.SendTo("prediction", "8080", "predict")
+	} else {
+		row := strings.Join([]string{age, sex, cp, trestbps, chol, fbs, hasHeartDisease}, ",")
+		db.AddRow(row)
+		log.Println("Row Added:", row)
+		messages.SendTo("prediction", "8080", "train")
+	}
+
 	http.Redirect(w, r, "/", http.StatusFound)
 }
