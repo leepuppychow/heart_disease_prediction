529093278ee6f9f4aba4103d1696d2cf4878b65f
leepuppychow <leepuppychow@gmail.com>
2019-03-08 01:16:12 -0700

    Can generate simple rectangle SVG in the server rendered form

--- visualization/histogram/histogram.go
+++ visualization/histogram/histogram.go
@@ -1,1 +1,17 @@
 package histogram
+
+import (
+	"net/http"
+
+	"github.com/ajstarks/svgo"
+)
+
+func Histogram(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "image/svg+xml")
+	s := svg.New(w)
+	s.Start(500, 500)
+	s.Rect(0, 0, 100, 20, "fill:red")
+	s.Rect(0, 20, 200, 20, "fill:blue")
+	s.Rect(0, 40, 150, 20, "fill:green")
+	s.End()
+}
--- visualization/main.go
+++ visualization/main.go
@@ -1,7 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+
+	"github.com/leepuppychow/heart_disease_prediction/visualization/histogram"
+)
 
 func main() {
-	fmt.Println("STARTING VISUALIZATION SERVICE")
+	startVisualizationService(":8888")
+}
+
+func startVisualizationService(port string) {
+	http.HandleFunc("/histogram", histogram.Histogram)
+	log.Println("Visualization service running on port", port)
+	log.Fatal(http.ListenAndServe(port, nil))
 }
