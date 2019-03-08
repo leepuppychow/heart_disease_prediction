9d772a6f566572d5ac8cd74fb486c7f8637e00e3
leepuppychow <leepuppychow@gmail.com>
2019-03-08 00:04:41 -0700

    Adds in a visualization service

--- prediction/handlers/handlers.go
+++ prediction/handlers/handlers.go
@@ -1,12 +1,16 @@
 package handlers
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+
+	"github.com/leepuppychow/heart_disease_prediction/prediction/database"
 )
 
 func TrainHandler(w http.ResponseWriter, r *http.Request) {
 	log.Println("HIT THE TRAIN HANDLER")
+	fmt.Println(database.GetAllRows("dataList"))
 }
 
 func PredictHandler(w http.ResponseWriter, r *http.Request) {
