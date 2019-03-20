0673fb119772f3a78e801ec1941b77520c9ee1eb
leepuppychow <leepuppychow@gmail.com>
2019-03-19 22:19:46 -0600

    Can generate histogram, but having bug with sending CSV contents to each service (file closed error)

--- visualization/histogram.py
+++ visualization/histogram.py
@@ -2,7 +2,16 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 
-def generate_histogram(data):
+def generate_histograms(data):
   df = pd.read_csv(data, sep=",")
-  print(df)
+  df.hist(bins=50, figsize=(20,15))
+  plt.savefig("./images/histograms.jpg", format="jpg")
+
+  # TODO: Maybe create separate histogram for each feature?
+  # for column in df:
+  #   feature = df[column]
+  #   feature.hist(bins=50)
+  #   filename = "./images/histogram-" + column + ".jpg"
+  #   plt.autoscale(enable=True, axis="both")
+  #   plt.savefig(filename, format="jpg")
   return
