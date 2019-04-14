d9260b0af7a8ee00b4d4190fdf2c8893db578e58
leepuppychow <leepuppychow@gmail.com>
2019-04-13 19:30:26 -0600

    Updates stats service to get Pearson corr coeff

--- statistics/app.py
+++ statistics/app.py
@@ -2,7 +2,7 @@ from flask import Flask, jsonify, request
 from flask_caching import Cache
 from flask_cors import CORS
 from io import StringIO
-import descriptive
+import stats
 
 config = {
     "DEBUG": True,
@@ -15,7 +15,8 @@ CORS(app)
 
 app.config.from_mapping(config)
 cache = Cache(app)
-cache.set("stats", "")
+cache.set("descriptive", "")
+cache.set("correlations", "")
 
 def convert_to_string(bytes_data):
   s = str(bytes_data, 'utf-8')
@@ -23,14 +24,19 @@ def convert_to_string(bytes_data):
 
 @app.route("/stats", methods=['GET'])
 def get_current_stats():
-  return cache.get("stats"), 200
+  stats_type = request.args.get("type")
+  data = cache.get(stats_type)
+  if data is None: 
+    return stats_type + " not found", 404
+  return data, 200
 
 @app.route("/stats", methods=['POST'])
 def create_new_descriptive_stats():
   data = convert_to_string(request.data)
-  stats = descriptive.update_stats(data)
-  cache.set('stats', stats) 
-  return stats, 200
+  descriptive, correlations = stats.update_stats(data)
+  cache.set("descriptive", descriptive) 
+  cache.set("correlations", correlations) 
+  return "Successfully updated statistics cache", 200
 
 if __name__ == "__main__":
   app.run(host="0.0.0.0", port=8111)
--- statistics/descriptive.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-
-def update_stats(data):
-  df = pd.read_csv(data, sep=",")
-  data = df.describe().to_json(orient="columns")
-  return data
